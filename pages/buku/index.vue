484c4f88e8170068de45b57e44eb93a17e941b23
Piaripiani01 <141712563+Piaripiani01@users.noreply.github.com>
2024-05-02 13:46:06 +0700

    Add files via upload

---
+++ pages/buku/index.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="container-fluid">
+        <div class="row content">
+            <div class="col-lg-12">
+                <h2 class="text-center my-4 just" style="color: aliceblue;">RAK BUKU</h2>
+                <div class="my-3 d-flex justify-content-center">
+                    <input type="search" class="form-control rounded-5 cari" placeholder="SEARCH?..." style="background-color: #B0CFE5;">
+                </div>
+                <div class="my-3 text-muted"></div>
+                <div class="row layer m-5 rounded-5">
+                    <h1 class="text-center" style="color: aliceblue;">Koleksi Buku</h1>
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/assets/img/k.jpg" class="cover" alt="cover 1" style="width: 100%;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/assets/img/p.jpg" class="cover" alt="cover 2" style="width: 100%;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/assets/img/n.jpeg" class="cover" alt="cover 3" style="width: 100%;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/assets/img/H.jpg" class="cover" alt="cover 3" style="width: 100%;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/assets/img/Y.jpg" class="cover" alt="cover 3" style="width: 100%;">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+
+.content {
+    background-color: #658694;
+}
+
+.cari{
+    width: 40rem;
+}
+/* .cover{
+    height: 50%;
+    width: 50%;
+} */
+.layer {
+    background-color: #89B6D6;
+}
+
+h2{
+    color: white;
+    font-family: "Irish Grover", system-ui;
+}
+
+h1{
+    color: white;
+    font-family: "Irish Grover", system-ui;
+}
+</style>
---
+++ pages/pengunjung/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container-fluid">
+        <div class="row content">
+            <div class="col-lg-12">
+                <nuxt-link to="/index"></nuxt-link>
+                <h2 class="text-center my-4">RIWAYAT PENGUNJUNG</h2>
+                <div class="row my-3 d-flex justify-content-center">
+                    <input type="search" class="col-lg-10 form-control- form-control-lg rounded-5 "placeholder="Search..." style="background-color: #B2CDE1;">
+                </div>
+                <div class="my-3 text-muted"></div>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <td>NO</td>
+                            <td>NAMA</td>
+                            <td>KATEGORI</td>
+                            <td>WAKTU/TANGGAL</td>
+                            <td>KEPERLUAN</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1.</td>
+                            <td>Pia ripiani</td>
+                            <td>siswa</td>
+                            <td>08.00/10-04-2024</td>
+                            <td>Membaca</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap');
+.content{
+    background-color: #658694;
+}
+td {
+    color: white;
+    border: 1px solid #fff;
+    background-color: rgba(255, 255, 255, 0);
+}
+
+h2{
+    color: white;
+    font-family: "Irish Grover", system-ui;
+}
+</style>
